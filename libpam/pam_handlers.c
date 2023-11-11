f365e959f1ac87b9667c995601388332c1460f65
Tobias Stoeckmann <tobias@stoeckmann.org>
2023-11-11 16:55:28 +0100

    libpam: make definition of _pam_load_module static
    The declaration already states that it is static. Make the definition
    static as well.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- libpam/pam_handlers.c
+++ libpam/pam_handlers.c
@@ -776,10 +776,10 @@ _pam_load_module(pam_handle_t *pamh, const char *mod_path, int handler_type)
     return success == PAM_SUCCESS ? mod : NULL;
 }
 
-int _pam_add_handler(pam_handle_t *pamh
-		     , int handler_type, int other, int stack_level, int type
-		     , int *actions, const char *mod_path
-		     , int argc, char **argv, int argvlen)
+static int _pam_add_handler(pam_handle_t *pamh
+			    , int handler_type, int other, int stack_level
+			    , int type, int *actions, const char *mod_path
+			    , int argc, char **argv, int argvlen)
 {
     struct loaded_module *mod = NULL;
     struct handler **handler_p;
